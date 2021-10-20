07e9219dc87ed7e52b0548189540e2bf9d5384fc
rohan4linux <39763276+rohan4linux@users.noreply.github.com>
2021-10-21 00:12:46 +0530

    added variables.tf

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "aws_region" {
+  default = "us-east-1"
+}
+
+variable "access_key" {
+  default = "AKIAQQ4UKHAXU5KZTFUS"
+}
+
+variable "secret_key" {
+  default = "mW0c6VqD1hY9FI/HbH74bL6VdUYdGTClvl/f5xDD"
+}
+
+variable "AWS_ECR_REGION" {
+  default = "us-east-1"
+}
