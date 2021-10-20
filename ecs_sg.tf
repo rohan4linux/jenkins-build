7e80f7fed0d856688cf55fb6b4a2440ab24b6cb3
rohan4linux <39763276+rohan4linux@users.noreply.github.com>
2021-10-21 00:16:17 +0530

    added ecs_sg

---
+++ ecs_sg.tf
@@ -0,0 +1,18 @@
+resource "aws_security_group" "hello_world_task" {
+  name        = "example-task-security-group"
+  vpc_id      = aws_vpc.ecs_vpc.id
+
+  ingress {
+    protocol        = "tcp"
+    from_port       = 80
+    to_port         = 80
+    security_groups = [aws_security_group.SG.id]
+  }
+
+  egress {
+    protocol    = "-1"
+    from_port   = 0
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
